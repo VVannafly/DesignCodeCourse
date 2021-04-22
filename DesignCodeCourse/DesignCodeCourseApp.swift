8e142e2b68ec196051eb5bd0b6c336c233bce114
Dmitry <41203105+VVannafly@users.noreply.github.com>
2021-04-22 19:01:47 +0300

    Initial Commit

---
+++ DesignCodeCourse/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  DesignCodeCourse
+//
+//  Created by Dmitriy Chernov on 22.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
