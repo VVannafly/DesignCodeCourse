c6f83649fec494c3cac19c92c9b61b963b32b163
Dmitry <41203105+VVannafly@users.noreply.github.com>
2021-04-22 19:20:22 +0300

    feat: Content Card component

--- DesignCodeCourse/ContentView.swift
+++ DesignCodeCourse/ContentView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                Spacer()
+            }
+            .frame(width: 300, height: 220)
+            .background(Color.blue)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
+                    }
+                    Spacer()
+                    Image("Logo1")
+                }
+                .padding()
+                
+
+                Image("Card1")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 300, height: 110, alignment: .top)
+            }
+            .frame(width: 340, height: 220)
+            .background(Color.black)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+        }
     }
 }
 
