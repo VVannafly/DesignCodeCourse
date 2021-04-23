96df09f99616a09f09cd4cd388174d1d172b250e
Dmitry <41203105+VVannafly@users.noreply.github.com>
2021-04-23 17:30:56 +0300

    feat: card view cards animation

---
+++ DesignCodeCourse/Views/CardView.swift
@@ -0,0 +1,44 @@
+//
+//  CardView.swift
+//  DesignCodeCourse
+//
+//  Created by Dmitriy Chernov on 23.04.2021.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding()
+
+
+            Image("Card1")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340, height: 220)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
