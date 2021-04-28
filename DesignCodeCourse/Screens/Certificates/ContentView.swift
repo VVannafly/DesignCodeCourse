2afb7c6fc7641ae565cd01cb8beec05ec7606e65
Dmitry <41203105+VVannafly@users.noreply.github.com>
2021-04-28 18:24:39 +0300

    fix: ring customisation and animation

--- DesignCodeCourse/Screens/Certificates/ContentView.swift
+++ DesignCodeCourse/Screens/Certificates/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @State var bottomState = CGSize.zero
     @State var showFull = false
 
+
     var body: some View {
         ZStack {
             TitleView()
@@ -87,7 +88,7 @@ struct ContentView: View {
                 )
 //            Text("\(bottomState.height)").offset(y: -300)
 
-            BottomSheetView()
+            BottomSheetView(show: $showCard)
                 .offset(x: 0, y: showCard ? 430 : 1000)
                 .offset(y: bottomState.height)
                 .blur(radius: show ? 20 : 0)
@@ -133,21 +134,4 @@ struct BackCardView: View {
         }
     }
 }
-
-struct TitleView: View {
-    var body: some View {
-        VStack {
-            HStack {
-                Text("Certificates")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                Spacer()
-            }
-            .padding()
-            Image("Background1")
-            Spacer()
-        }
-    }
-}
-
 
---
+++ DesignCodeCourse/Screens/Certificates/Views/TitleView.swift
@@ -0,0 +1,30 @@
+//
+//  TitleView.swift
+//  DesignCodeCourse
+//
+//  Created by Dmitriy Chernov on 28.04.2021.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct TitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleView()
+    }
+}
