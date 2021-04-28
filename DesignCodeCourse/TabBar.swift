5501fa178aa38d734a59c95e5abc61bebc7849ba
Dmitry <41203105+VVannafly@users.noreply.github.com>
2021-04-28 17:28:08 +0300

    feat: progress ring view

---
+++ DesignCodeCourse/TabBar.swift
@@ -0,0 +1,33 @@
+//
+//  TabBar.swift
+//  DesignCodeCourse
+//
+//  Created by Dmitriy Chernov on 27.04.2021.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            HomeScreen().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            TabBar().previewDevice("iPhone 8")
+            TabBar().previewDevice("iPhone 11 Pro Max")
+        }
+
+    }
+}
