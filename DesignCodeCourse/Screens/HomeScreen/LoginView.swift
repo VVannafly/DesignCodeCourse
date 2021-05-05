c4fd35ad1f257b3fe071335fee924259816c22d2
Dmitry <wodi220500@gmail.com>
2021-05-05 19:01:03 +0300

    feat: login screen paralax animation

--- DesignCodeCourse/Screens/HomeScreen/LoginView.swift
+++ DesignCodeCourse/Screens/HomeScreen/LoginView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var show = false
+    @State var viewState = CGSize.zero
+    @State var isDragging = false
+    
     var body: some View {
         ZStack(alignment: .top) {
             Color.black.edgesIgnoringSafeArea(.all)
@@ -24,32 +28,59 @@ struct LoginView: View {
                 }
                 .frame(maxWidth: 375, maxHeight: 100)
                 .padding(.horizontal, 16)
+                .offset(x: viewState.width / 15, y: viewState.height / 15)
                 
                 Text("80 hours of courses for SwiftUI, React and design tools.")
                     .font(.headline)
                     .frame(width: 250)
-                
+                    .offset(x: viewState.width / 15, y: viewState.height / 15)
                 Spacer()
-                    
+                
             }
             .multilineTextAlignment(.center)
             .padding(.top, 100)
-            .frame(width: 477)
+            .frame(width: 407)
             .frame(maxWidth: .infinity)
             .background(
                 ZStack {
                     Image(uiImage: #imageLiteral(resourceName: "Blob"))
                         .offset(x: -150, y: -200)
+                        .rotationEffect(.degrees(show ? 360 + 90 : 90))
                         .blendMode(.plusDarker)
+                        .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false))
+                        .onAppear {
+                            self.show = true
+                        }
                     
                     Image(uiImage: #imageLiteral(resourceName: "Blob"))
                         .offset(x: -200, y: -250)
+                        .rotationEffect(.degrees(show ? 360 : 0), anchor: .leading)
                         .blendMode(.overlay)
+                        .animation(Animation.linear(duration: 100).repeatForever(autoreverses: false))
                 }
             )
-            .background(Image(uiImage: #imageLiteral(resourceName: "Card5")), alignment: .bottom)
+            .background(
+                Image(uiImage: #imageLiteral(resourceName: "Card3"))
+                    .offset(x: viewState.width / 25, y: viewState.height / 25),
+                alignment: .bottom)
             .background(Color(#colorLiteral(red: 0.4117647059, green: 0.4705882353, blue: 0.9725490196, alpha: 1)))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .scaleEffect(isDragging ? 0.9 : 1)
+            .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+            .rotation3DEffect(
+                .degrees(5),
+                axis: (x: viewState.width, y: viewState.height, z: 0.0))
+            
+            .gesture(
+                DragGesture().onChanged { value in
+                    self.viewState = value.translation
+                    self.isDragging = true
+                }
+                .onEnded { value in
+                    self.viewState = .zero
+                    self.isDragging = false
+                }
+            )
         }
     }
 }
@@ -57,6 +88,6 @@ struct LoginView: View {
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
-//            .previewDevice("iPhone SE (2nd generation)")
+        //            .previewDevice("iPhone SE (2nd generation)")
     }
 }
